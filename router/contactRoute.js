af68e7430d5d2d91985aa88e33178a4351d60206
Ansh Talaviya <anshtalaviya22@gmail.com>
2025-05-21 15:32:40 +0530

    init

--- router/contactRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const contactController = require("../controller/contactController");
-
-// Access the function inside the object
-router.post("/contact", contactController.submitForm);
-
-module.exports = router;
