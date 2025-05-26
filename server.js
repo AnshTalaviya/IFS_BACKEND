81734724acc9e11a23d872d5be936f4fd716bd2c
Ansh Talaviya <anshtalaviya22@gmail.com>
2025-05-26 18:25:43 +0530

    init

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const sendDetailsRoute = require("./router/sendDetailsRouter");
 
 const app = express();
 
-app.use(cors());
+app.use(cors("*"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -33,7 +33,7 @@ mongoose.connect(process.env.MONGO_URI, {
 .catch((err) => console.error("MongoDB connection error:", err));
 
 // Use routes
-app.use("/api", authRoutes);
+app.use("/api", authRoutes);    
 app.use("/api/personal_details", personalDetailsRouter);
 app.use("/api", sendDetailsRoute);
 app.use("/api/investment_preferences", investmentPreferencesRouter);
